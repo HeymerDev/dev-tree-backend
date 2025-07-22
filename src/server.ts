5977246794193a1b317b5a68209feea570b0b3ca
HeymerDev <heymermeza11@gmail.com>
2025-07-21 23:06:38 -0500

    resolving cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import cors from "cors";
 import router from "./router";
 import { connectDB } from "./config/db";
 import dotenv from "dotenv";
+import { corsConfig } from "./config/cors";
 
 dotenv.config(); // Load environment variables from .env file
 export const app = express();
+
+app.use(cors(corsConfig))
 connectDB(); // Connect to MongoDB
 
 app.use(express.json());
